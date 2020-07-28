e66b6f15b92d067f465b8c40a97f0577d79f8d06
zxfjd3g <258147149@qq.com>
2020-07-28 12:01:21 +0800

    -3. 组件与Vue的关系理解

---
+++ VueComponent/Test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>{{title}}</h2>
+  </div>
+</template>
+
+<script>
+export default { // 默认暴露配置对象
+  data() {
+    return {
+      title: 'Test 组件标题'
+    }
+  },
+}
+</script>
+
+<style>
+  h2 {
+    color: red;
+  }
+</style>
