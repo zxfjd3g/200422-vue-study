c7b6a512a761a71cd8f70960e547d7ac19f8de6c
zxfjd3g <258147149@qq.com>
2020-08-01 17:22:16 +0800

    - 35. 自定义事件总线

--- VueComponent/custom/eventBus.js
+++ VueComponent/custom/eventBus.js
@@ -23,27 +23,49 @@
     'delete': [listener3]
   }
   */
+  let listnerContainer = {}
   
 
   /* 
   1. on(eventName, listener): 绑定事件监听
   */
   eventBus.on = function (eventName, listener) {
-    
+
+    // 先根据eventName获取对应的数组小容器
+    const listeners = listnerContainer[eventName] 
+    // 如果不存在
+    if (!listeners) {
+      listnerContainer[eventName] = [listener]
+    } else { // 如果存在
+      listeners.push(listener)
+    }
+    // console.log('on()', listnerContainer)
   }
   
   /* 
   2. emit/dispatch(eventName, data): 分发事件
   */
   eventBus.emit = function (eventName, data) {
-      
+    // 根据eventName得到包含所有对应回调函数的数组容器
+    const listeners = listnerContainer[eventName]
+    // 如果有, 遍历每个listener, 立即同步调用它
+    if (listeners && listeners.length>0) {
+      listeners.forEach(listener => listener(data))
+    }
   }
 
   /* 
   3. off(eventName): 解绑事件监听
+    如果eventName没指定, 解绑所有监听
   */
   eventBus.off = function (eventName) {
-      
+    if (eventName===undefined) { // 解绑所有监听
+      listnerContainer = {}
+    } else if (typeof eventName==='string') {
+      delete listnerContainer[eventName] // 删除对应的属性
+    } else {
+      throw new Error('事件名可以不传或者为字符串')
+    }
   }
 
   // 暴露总线对象
